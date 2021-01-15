b0731bda603600f31875ef4f6b74ff4c9104e0ac
Hugo Bollon <hugo.bollon@gmail.com>
2021-01-15 13:56:59 +0100

    Add Soap request on item-card to get delivery fees

--- src/app/item-card/item-card.component.ts
+++ src/app/item-card/item-card.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
+const soapRequest: any = require('easy-soap-request');
+
 @Component({
   selector: 'app-item-card',
   templateUrl: './item-card.component.html',
@@ -7,9 +9,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ItemCardComponent implements OnInit {
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit(): void {
+
   }
+
+  getDeliveryFee(weight : number) : void {
+    const url = 'http://localhost:8080/MarketplaceServer-1.0-SNAPSHOT/services/DeliveryFee?wsdl';
+    const sampleHeaders = {
+      'Content-Type': 'text/xml;charset=UTF-8',
+    };
+    const xml = `<soapenv:Envelope xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/" xmlns:mar="http://MarketplaceServer.bitsplease.com/">
+    <soapenv:Header/>
+      <soapenv:Body>
+        <mar:CalculateDelivery>
+          <arg0>` + weight + `</arg0>
+        </mar:CalculateDelivery>
+      </soapenv:Body>
+    </soapenv:Envelope>`;
+
+    (async () => {
+      const { response } = await soapRequest({ url: url, headers: sampleHeaders, xml: xml, timeout: 1000 }); // Optional timeout parameter(milliseconds)
+      const { headers, body, statusCode } = response;
+      console.log(headers);
+      console.log(body);
+      console.log(statusCode);
+    })();
+
+   }
 
 }
